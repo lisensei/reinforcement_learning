81c6cdab3c80ffb140dc2931df276af1f3d28541
unknown <leesensei@leesensei.com>
2022-05-03 12:45:06 -0700

    better structure

--- action_value_policy_iteration.py
+++ action_value_policy_iteration.py
@@ -2,43 +2,45 @@ import matplotlib.text
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import *
+from itertools import *
 
 grid_world = np.arange(16).reshape(4, 4)
 values = np.array(grid_world, dtype=str)
 text_array = [0] * 16
-state_action_state = namedtuple("sas", ["state", "action_next_state"])
+state_action_next_state = namedtuple("sas", ["state_action", "next_state"])
 action_space = np.arange(4)
+state_action = product(grid_world.reshape(-1), action_space)
 states = []
-for (i, j), v in np.ndenumerate(grid_world):
-    if i - 1 >= 0:
-        up = [0, v - 4]
-    else:
-        up = [0, v]
-    if i + 1 <= 3:
-        down = [1, v + 4]
-    else:
-        down = [1, v]
-    if j - 1 >= 0:
-        left = [2, v - 1]
-    else:
-        left = [2, v]
-    if j + 1 <= 3:
-        right = [3, v + 1]
-    else:
-        right = [3, v]
-    action_next_states = [up, down, left, right]
-    states.append(state_action_state(v, action_next_states))
+for sa in state_action:
+    row = sa[0] // 4
+    col = sa[0] % 4
+    next_state = sa[0]
+    if sa[1] == 0:
+        if row - 1 >= 0:
+            next_state = sa[0] - 4
+    if sa[1] == 1:
+        if row + 1 <= 3:
+            next_state = sa[0] + 4
+    if sa[1] == 2:
+        if col - 1 >= 0:
+            next_state = sa[0] - 1
+    if sa[1] == 3:
+        if col + 1 <= 3:
+            next_state = sa[0] + 1
+    states.append(state_action_next_state(sa, next_state))
 
-for s in states:
-    print(s.state, s.action_next_state)
+for sans in states:
+    print(sans)
 
-with plt.ion():
-    plt.matshow(grid_world)
-    plt.show()
-    for (i, j), _ in np.ndenumerate(grid_world):
-        text_array[i * 4 + j] = plt.text(i, j, "0")
-    for s in range(10):
-        plt.axis("off")
-        for (i, j), v in np.ndenumerate(grid_world):
-            text_array[i * 4 + j].set(text=str(np.random.randint(0, 200)))
-        plt.pause(1)
+
+def canvas(gw=grid_world):
+    with plt.ion():
+        plt.matshow(grid_world)
+        plt.show()
+        for (i, j), _ in np.ndenumerate(grid_world):
+            text_array[i * 4 + j] = plt.text(i, j, "0")
+        for s in range(10):
+            plt.axis("off")
+            for (i, j), v in np.ndenumerate(grid_world):
+                text_array[i * 4 + j].set(text=str(np.random.randint(0, 200)))
+            plt.pause(1)
