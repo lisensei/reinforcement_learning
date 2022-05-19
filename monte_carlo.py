08eed97f52e877f0d65696153f151433f86e7055
Sensei Lee <sherlocksolution@gmail.com>
2022-05-20 00:14:29 +0200

    polish

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,9 +1,12 @@
 """
-Monte Carlo estimation on 4x4 grid world
+Monte Carlo value iteration on 4x4 grid world
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
+
+'''This class creates the grid world environment'''
 
 
 class GridWorld:
@@ -55,15 +58,18 @@ class GridWorld:
         return self.initial_state
 
 
+'''This class creates an agent'''
+
+
 class Agent:
-    def __init__(self, environment, num_states=16, num_actions=4):
+    def __init__(self, num_states=16, num_actions=4):
         self.experience = []
         self.grid_world = np.arange(0, 16).reshape(-1, 4)
         self.policy = np.ones((num_states, num_actions)) / num_actions
         self.state_value_estimates = np.zeros(num_states)
         self.state_update_count = np.zeros(num_states)
         self.legal_moves = self.get_legal_moves()
-        self.env = environment
+        self.policy_update_count = 0
 
     def get_episode(self, env):
         cs = env.current_state
@@ -78,7 +84,7 @@ class Agent:
             rewards.append(*rwd)
         return np.array(trajectory), np.array(rewards)
 
-    def learning_with_monte_carlo(self):
+    def update_value_estimates(self):
         gamma = np.array(1)
         env = GridWorld()
         env.init_env()
@@ -93,14 +99,22 @@ class Agent:
                 self.state_value_estimates[i] = (self.state_value_estimates[i] * self.state_update_count[
                     i] + cum_return) / (self.state_update_count[i] + 1)
                 self.state_update_count[i] += 1
-        self.update_policy()
 
-    def update_policy(self, eps=0.01):
+    '''Epsilon greedy and set equal probability for equal state values to avoid infinite loop'''
+
+    def update_policy(self, eps=0.1):
+        eps = eps ** (1 + self.policy_update_count / 1000) if eps != 0 else eps
         for j in range(1, 15):
             reachable_state_value = self.state_value_estimates[self.legal_moves[j, :]]
-            optimal_action = np.argmax(reachable_state_value)
-            self.policy[j, :] = np.ones((1, 4)) * eps / 3
-            self.policy[j, optimal_action] = 1 - eps
+            max_index = np.argmax(reachable_state_value)
+            optimal_action = np.argwhere(reachable_state_value == reachable_state_value[max_index])
+            num_optimal_actions = len(optimal_action)
+            if num_optimal_actions < 4:
+                self.policy[j, :] = np.ones((1, 4)) * eps / (4 - num_optimal_actions)
+                self.policy[j, optimal_action] = (1 - eps) / num_optimal_actions
+            else:
+                self.policy[j, :] = np.ones((1, 4)) / 4
+        self.policy_update_count += 1
 
     def get_legal_moves(self):
         legal_moves = np.zeros((1, 4), dtype=np.int32)
@@ -120,18 +134,21 @@ class Agent:
         return legal_moves
 
 
+'''This class is used to visualize state values and policy'''
+
+
 class Canvas:
     def __init__(self, shape):
         self.fig, self.axes = plt.subplots(1, 2, layout="constrained")
-        initial_values = np.arange(16).reshape(shape).transpose()
-        '''set state values axes'''
+        initial_values = np.zeros(16).reshape(shape).transpose()
+        '''Set state values axes'''
         self.axes[0].axis("off")
         self.axes[0].set_title("State Values Estimates")
         self.axes[0].xaxis.tick_top()
         self.axes[0].invert_yaxis()
         self.axes[0].matshow(initial_values, cmap="Blues")
 
-        '''set policy axes'''
+        '''Set policy axes'''
         self.axes[1].set_title("Policy")
         self.axes[1].axis("off")
         self.axes[1].xaxis.tick_top()
@@ -152,6 +169,8 @@ class Canvas:
                                    length_includes_head=False)
                 self.axes[1].arrow(i, j, 0, -0.1, shape="full", head_width=self.arrow_head_width, color="green",
                                    length_includes_head=False)
+
+    '''Plot state values and policy. Only one policy is plotted'''
 
     def repaint(self, values, policy):
         self.axes[0].clear()
@@ -179,13 +198,11 @@ class Canvas:
                     self.axes[1].arrow(i, j, -0.1, 0, shape="full", head_width=self.arrow_head_width,
                                        head_length=self.arrow_head_length, color="green", length_includes_head=False)
                 if v == 3:
-                    self.axes[1].arrow(i, j, 0, -0.1, shape="full", head_width=self.arrow_head_width,
+                    self.axes[1].arrow(i, j, 0.1, 0, shape="full", head_width=self.arrow_head_width,
                                        head_length=self.arrow_head_length, color="green", length_includes_head=False)
 
 
-gw = GridWorld()
-gw.init_env()
-agent = Agent(gw)
+agent = Agent()
 plt.ion()
 can = Canvas((4, 4))
 plt.figure(can.fig)
@@ -197,4 +214,5 @@ for i in range(20000):
         policy = np.argmax(agent.policy, axis=1).reshape(-1, 4).transpose()
         can.repaint(values, policy)
         plt.pause(0.5)
-    agent.learning_with_monte_carlo()
+    agent.update_value_estimates()
+    agent.update_policy()
